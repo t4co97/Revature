194251b134bb99ffef9c78af2b89acd71d91971a
t4co97 <tylerjairon@yahoo.com>
2021-01-18 22:50:54 -0600

    sixth

--- project0/src/main/scala/com/revature/journalapp/cli/Cli.scala
+++ project0/src/main/scala/com/revature/journalapp/cli/Cli.scala
@@ -7,6 +7,8 @@ import com.revature.journalapp.utils
 import com.revature.journalapp.dao.JournalDao
 import com.revature.journalapp.model.Journal
 import java.io.File
+import scala.collection.mutable.ArrayBuffer
+import java.sql.Date
 
   class Cli {
     
@@ -15,11 +17,14 @@ import java.io.File
 
 
       def printWelcome(): Unit = {
-         println("Welcome to your journal")
+         println("Welcome to Your Journal")
     }
     
       def printOptions(): Unit = {
           println("Previous Entries: p")
+          println("Previous Entries Ordered by Date: o")
+          println("Search by ID: s")
+          println("Search by Date: sd")
           println("Make New Entry: n")
           println("Edit Entry: e")
           println("Delete Entry: d")
@@ -34,14 +39,79 @@ import java.io.File
         println(s"Entry: ${journal(i).entry}")
         println()  
         }
-       
         
-       
-        
+      }
+
+      def entriesByDate(): Unit = {
+        val journal = JournalDao.getAllbyDate()
+        for(i <- 0 to (journal.length-1)){
+        println(s"Entry ID: ${journal(i).entryId}")
+        println(s"Entry Date: ${journal(i).entryDate}")
+        println(s"Entry: ${journal(i).entry}")
+        println()  
+        }
+      }
+
+      def searchID(): Unit = {
+        var entryId = 0
+        var eCaught = false
+
+        println("Please Type ID Number")
+         try{
+          entryId = StdIn.readLine().toInt
+        }catch{
+          case n: NumberFormatException =>
+          println("PLEASE USE ID NUMBER WHEN SEARCHING")
+          eCaught = true
+        }finally{
+          if(eCaught){
+            return
+          }
+          val journal = JournalDao.searchByID(entryId)
+          if(journal.isEmpty){
+            println("NO ENTRY WITH THAT ID")
+            return
+          }
+          for(i <- 0 to (journal.length-1)){
+          println(s"Entry ID: ${journal(i).entryId}")
+          println(s"Entry Date: ${journal(i).entryDate}")
+          println(s"Entry: ${journal(i).entry}")
+          println()  
+          }
+        }
+      }
+
+      def searchDate(): Unit = {
+        var entryDate: Date = null
+        var eCaught = false
+
+        println("Please Type Date: Format(YYYY-MM-DD)")
+         try{
+          entryDate = java.sql.Date.valueOf(StdIn.readLine())
+        }catch{
+          case e: Exception =>
+          println("PLEASE USE CORRECT DATE FORMAT")
+          eCaught = true
+        }finally{
+          if(eCaught){
+            return
+          }
+          val journal = JournalDao.searchByDate(entryDate)
+          if(journal.isEmpty){
+            println("NO ENTRY WITH THAT DATE")
+            return
+          }
+          for(i <- 0 to (journal.length-1)){
+          println(s"Entry ID: ${journal(i).entryId}")
+          println(s"Entry Date: ${journal(i).entryDate}")
+          println(s"Entry: ${journal(i).entry}")
+          println()  
+          }
+        }
       }
       
       def makeNewEntry(): Unit  = {
-        println("Choose File to Upload")
+        println("Choose CSV File to Upload: Format(date,entry)")
 
         val filename = StdIn.readLine();
 
@@ -54,77 +124,112 @@ import java.io.File
         }
         else{
           println("FILE DOES NOT EXIST")
-          println(s"""Found top level files:
+          println(s"""Found Top Level Files:
               ${FileUtil.getTopLevelFiles.mkString(", ")}""")
         }
       }
 
       def editEntry(): Unit ={
-        JournalDao.getAll().foreach(println)
-        println("Choose Entry to Edit: Use entry id to choose")
-        val entryId = StdIn.readLine()
-        println("Change Date, Entry, or Both?")
-        
-        val input = StdIn.readLine()
-        input match{
-            case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("date") => {
-              editDate()
-            }
-            case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("entry") => {
-              editOnlyEntry()
-            }
-            case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("both") => {
-              editEverything()
-            }
-          }
-      }
-      
-      def editDate(): Unit = {
-
-      }
-
-      def editOnlyEntry(): Unit = {
-
-      }
-
-      def editEverything(): Unit = {
-        println("Choose File to Replace Current Entry")
-
-        val filename = StdIn.readLine();
-
-        if (new java.io.File(filename).isFile()){
-          if(JournalDao.addEntry(FileUtil.getTextContent(filename))){
-            println("Added Entry")
-          }else{
-            println("Failed to Add Entry!")
-          }
-        }
-        else{
-          println("FILE DOES NOT EXIST")
-          println(s"""Found top level files:
-              ${FileUtil.getTopLevelFiles.mkString(", ")}""")
-        }
-      }
-
-      def deleteEntry():Unit = {
         printPreviousEntries()
-        println("Choose Entry to Delete: Use entry id to choose")
-        var entryId: Int = 0
+        println("Choose Entry to Edit: USE ENTRY ID TO CHOOSE")
+        var entryData: (Int, Int, String, String) = null
+        var eCaught: Boolean = false
+        var entryId:Int = 0
+        var entryEdit: Int = 0
         try{
           entryId = StdIn.readLine().toInt
-          println(s"ARE YOU SURE YOU WANT TO DELETE ENTRY $entryId y or n")
-        if(StdIn.readLine.equalsIgnoreCase("y")){
-          if(JournalDao.delteEntry(entryId)){
-            println("Successfully Deleted")
-          }else{
-            println("Failed to Delete")
-          } 
-        }else {
-        
-        }
         }catch{
           case n: NumberFormatException =>
           println("PLEASE USE ID NUMBER WHEN PICKING")
+          eCaught = true
+        }finally{
+          if(eCaught){
+            return
+          }
+          var loop: Boolean = true
+          
+          while(loop){
+          println("Change Date, Entry, or Both? or Exit")
+
+          
+          val input = StdIn.readLine()
+          input match{
+            case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("date") => {
+              entryEdit = 0
+              loop = false
+            }
+            case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("entry") => {
+              entryEdit = 1
+              loop = false
+            }
+            case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("both") => {
+              entryEdit = 2
+              loop = false
+            }
+            case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("exit") => {
+              return
+            }  
+            case _ => {
+              println("Failed to parse any input")
+            }
+         
+            }
+          }
+          
+          println("Choose File to Replace Current Entry")
+
+                val filename = StdIn.readLine();
+
+                if (new java.io.File(filename).isFile()){
+                  var data = FileUtil.getTextContent(filename)
+                  entryData = (entryId, entryEdit, data(0), data(1))
+                  if(JournalDao.editEntry(entryData)){
+                    println("Updated Entry")
+                  }else{
+                    println("Failed to Update Entry!")
+                  }
+                }
+                else{
+                  println("FILE DOES NOT EXIST")
+                  println(s"""Found top level files:
+                      ${FileUtil.getTopLevelFiles.mkString(", ")}""")
+                }
+              
+
+          
+          }
+        
+      }
+      
+     
+
+      def deleteEntry():Unit = {
+        var entryId: Int = 0
+        var eCaught: Boolean = false
+
+        printPreviousEntries()
+        println("Choose Entry to Delete: Use entry id to choose")
+
+        try{
+          entryId = StdIn.readLine().toInt
+        }catch{
+          case n: NumberFormatException =>
+          println("PLEASE USE ID NUMBER WHEN PICKING")
+          eCaught =true
+        }finally{
+          if(eCaught){
+            return
+          }
+          println(s"ARE YOU SURE YOU WANT TO DELETE ENTRY $entryId y or n")
+          if(StdIn.readLine.equalsIgnoreCase("y")){
+            if(JournalDao.delteEntry(entryId)){
+              println("Successfully Deleted")
+            }else{
+              println("Failed to Delete")
+            } 
+          }else {
+          
+          }
         }    
         
       }
@@ -142,6 +247,15 @@ import java.io.File
            
             case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("p") => {
               printPreviousEntries()
+            }
+            case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("o") => {
+              entriesByDate()
+            }
+            case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("s") => {
+              searchID()
+            }
+            case commandArgPattern(cmd, arg)if cmd.equalsIgnoreCase("sd") => {
+              searchDate()
             }
             case commandArgPattern(cmd, arg) if cmd.equalsIgnoreCase("n") => {
               makeNewEntry()
--- project0/src/main/scala/com/revature/journalapp/dao/JournalDao.scala
+++ project0/src/main/scala/com/revature/journalapp/dao/JournalDao.scala
@@ -4,6 +4,8 @@ import com.revature.journalapp.model.Journal
 import com.revature.journalapp.utils.ConnectionUtil
 import scala.util.Using
 import scala.collection.mutable.ArrayBuffer
+import java.sql.Date
+import java.sql.PreparedStatement
 
 
 
@@ -14,7 +16,7 @@ object JournalDao {
     def getAll(): Seq[Journal] = {
     val conn = ConnectionUtil.getConnection();
     Using.Manager { use =>
-        val stmt = use(conn.prepareStatement("SELECT * FROM project0.journal;"))
+        val stmt = use(conn.prepareStatement("SELECT * FROM project0.journal ORDER BY entry_id;"))
         stmt.execute()
         val rs = use(stmt.getResultSet())
         val allEntries: ArrayBuffer[Journal] = ArrayBuffer()
@@ -25,31 +27,104 @@ object JournalDao {
         }.get
     }
 
+    def getAllbyDate(): Seq[Journal] = {
+    val conn = ConnectionUtil.getConnection();
+    Using.Manager { use =>
+        val stmt = use(conn.prepareStatement("SELECT * FROM project0.journal ORDER BY entry_date;"))
+        stmt.execute()
+        val rs = use(stmt.getResultSet())
+        val allEntries: ArrayBuffer[Journal] = ArrayBuffer()
+        while (rs.next()) {
+            allEntries.addOne(Journal.fromResultSet(rs))
+        }
+        allEntries.toList
+        }.get
+    }
+
+    def searchByID(entryID: Int): Seq[Journal] = {
+    val conn = ConnectionUtil.getConnection();
+    Using.Manager { use =>
+        val stmt = use(conn.prepareStatement("SELECT * FROM project0.journal WHERE entry_id = ?;"))
+        stmt.setInt(1, entryID)
+        stmt.execute()
+        val rs = use(stmt.getResultSet())
+        val allEntries: ArrayBuffer[Journal] = ArrayBuffer()
+        while (rs.next()) {
+            allEntries.addOne(Journal.fromResultSet(rs))
+        }
+        allEntries.toList
+        }.get
+    }
+
+    def searchByDate(entryDate: Date): Seq[Journal] = {
+    val conn = ConnectionUtil.getConnection();
+    Using.Manager { use =>
+        val stmt = use(conn.prepareStatement("SELECT * FROM project0.journal WHERE entry_date = ?;"))
+        stmt.setDate(1, entryDate)
+        stmt.execute()
+        val rs = use(stmt.getResultSet())
+        val allEntries: ArrayBuffer[Journal] = ArrayBuffer()
+        while (rs.next()) {
+            allEntries.addOne(Journal.fromResultSet(rs))
+        }
+        allEntries.toList
+        }.get
+    }
+
+
     def addEntry(entry : Array[String]): Boolean = {
         val conn = ConnectionUtil.getConnection();
         Using.Manager { use =>
         val insertstmt = use(conn.prepareStatement("INSERT INTO project0.journal VALUES (DEFAULT, ?, ?);"))
-        insertstmt.setDate(1, java.sql.Date.valueOf(entry(0)))
+        var entryDate: Date = null
+        try{
+        entryDate = java.sql.Date.valueOf(entry(0))
+        insertstmt.setDate(1, entryDate)
         insertstmt.setString(2, entry(1))    
         insertstmt.execute()
-            
+        }catch{
+            case e: Exception =>
+            println("DATE NOT FORMATED CORRECTLY YYYY-MM-DD")
+        }    
         insertstmt.getUpdateCount() > 0
         }.getOrElse(false)
     }
     
-    def editEntry(entry : Array[String]): Boolean = {
+    def editEntry(entry : (Int, Int, String, String)): Boolean = {
         val conn = ConnectionUtil.getConnection();
         Using.Manager { use =>
-        val stmt = use(conn.prepareStatement("UPDATE project0.journal SET entry_date = ?, entry = ? WHERE entry_id = ?;"))
-        stmt.setString(1, entry(0))
-        stmt.setString(2, entry(1))  
-        stmt.setString(2, entry(1))   
-        stmt.execute()
+        var stmt: PreparedStatement = use(conn.prepareStatement("UPDATE project0.journal SET entry_date = ?, entry = ? WHERE entry_id = ?;"))    
+        var entryDate: Date = null
+        try{
+            entryDate = java.sql.Date.valueOf(entry._3)
+        }catch{
+            case e: Exception =>
+            println("DATE NOT FORMATED CORRECTLY YYYY-MM-DD")
+        }finally {
+            if(entry._2 == 0){
+                stmt = use(conn.prepareStatement("UPDATE project0.journal SET entry_date = ? WHERE entry_id = ?;"))
+                stmt.setDate(1, entryDate)
+                stmt.setInt(2, entry._1)
+                stmt.execute()  
+            }else if(entry._2 == 1){
+                stmt = use(conn.prepareStatement("UPDATE project0.journal SET entry = ?  WHERE entry_id = ?;"))
+                stmt.setString(1, entry._4)
+                stmt.setInt(2, entry._1)    
+                stmt.execute()
+            }else{ 
+                stmt.setDate(1, entryDate)
+                stmt.setString(2, entry._4)
+                stmt.setInt(3, entry._1)    
+                stmt.execute()
+            }
             
+        }    
         stmt.getUpdateCount() > 0
         }.getOrElse(false)
     }
 
+
+    
 
     def delteEntry(entry : Int): Boolean = {
         val conn = ConnectionUtil.getConnection();
